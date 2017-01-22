e220d31f7a9d112c72745f711216a4e68c85bb20
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2017-01-22 18:13:32 +0900

    デバッグログつけ

--- Sudoku.rb
+++ Sudoku.rb
@@ -1,12 +1,29 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
+require 'pp'
+
 module Sudoku
+  class Group < Array
+    attr_reader :id
+    def initialize(id, *args)
+      @id = id
+      args.each do |el|
+        self << el
+      end
+    end
+  end
+
   class Cell
+    attr_reader   :x, :y
     attr_accessor :value
+    attr_accessor :groups
     attr_accessor :candidates
-    def initialize
+    def initialize(x, y)
+      @x = x
+      @y = y
       @value = 0
+      @groups = []
       @candidates = [1,2,3,4,5,6,7,8,9]
     end
   end
@@ -15,45 +32,62 @@ module Sudoku
     attr_reader :value_changed
 
     def initialize
-      @cell  = Array.new(81)
-      @group = Array.new(9+9+9)
+      @cell  = []
+      @group = []
 
-      81.times do |n|
-        @cell[n] = Cell.new
+      9.times do |y|
+        9.times do |x|
+          @cell << Cell.new(x,y)
+        end
       end
 
-      # row
-      @group[ 0] = [@cell[ 0], @cell[ 1], @cell[ 2], @cell[ 3], @cell[ 4], @cell[ 5], @cell[ 6], @cell[ 7], @cell[ 8]]
-      @group[ 1] = [@cell[ 9], @cell[10], @cell[11], @cell[12], @cell[13], @cell[14], @cell[15], @cell[16], @cell[17]]
-      @group[ 2] = [@cell[18], @cell[19], @cell[20], @cell[21], @cell[22], @cell[23], @cell[24], @cell[25], @cell[26]]
-      @group[ 3] = [@cell[27], @cell[28], @cell[29], @cell[30], @cell[31], @cell[32], @cell[33], @cell[34], @cell[35]]
-      @group[ 4] = [@cell[36], @cell[37], @cell[38], @cell[39], @cell[40], @cell[41], @cell[42], @cell[43], @cell[44]]
-      @group[ 5] = [@cell[45], @cell[46], @cell[47], @cell[48], @cell[49], @cell[50], @cell[51], @cell[52], @cell[53]]
-      @group[ 6] = [@cell[54], @cell[55], @cell[56], @cell[57], @cell[58], @cell[59], @cell[60], @cell[61], @cell[62]]
-      @group[ 7] = [@cell[63], @cell[64], @cell[65], @cell[66], @cell[67], @cell[68], @cell[69], @cell[70], @cell[71]]
-      @group[ 8] = [@cell[72], @cell[73], @cell[74], @cell[75], @cell[76], @cell[77], @cell[78], @cell[79], @cell[80]]
+      27.times do |n|
+        @group << Group.new(n)
+      end
 
-      # col
-      @group[ 9] = [@cell[ 0], @cell[ 9], @cell[18], @cell[27], @cell[36], @cell[45], @cell[54], @cell[63], @cell[72]]
-      @group[10] = [@cell[ 1], @cell[10], @cell[19], @cell[28], @cell[37], @cell[46], @cell[55], @cell[64], @cell[73]]
-      @group[11] = [@cell[ 2], @cell[11], @cell[20], @cell[29], @cell[38], @cell[47], @cell[56], @cell[65], @cell[74]]
-      @group[12] = [@cell[ 3], @cell[12], @cell[21], @cell[30], @cell[39], @cell[48], @cell[57], @cell[66], @cell[75]]
-      @group[13] = [@cell[ 4], @cell[13], @cell[22], @cell[31], @cell[40], @cell[49], @cell[58], @cell[67], @cell[76]]
-      @group[14] = [@cell[ 5], @cell[14], @cell[23], @cell[32], @cell[41], @cell[50], @cell[59], @cell[68], @cell[77]]
-      @group[15] = [@cell[ 6], @cell[15], @cell[24], @cell[33], @cell[42], @cell[51], @cell[60], @cell[69], @cell[78]]
-      @group[16] = [@cell[ 7], @cell[16], @cell[25], @cell[34], @cell[43], @cell[52], @cell[61], @cell[70], @cell[79]]
-      @group[17] = [@cell[ 8], @cell[17], @cell[26], @cell[35], @cell[44], @cell[53], @cell[62], @cell[71], @cell[80]]
+      81.times do |n|
+        # row
+        @group[n/9]     << @cell[n]
+        # col
+        @group[9 + n%9] << @cell[n]
+      end
 
       # 3x3 block
-      @group[18] = [@cell[ 0], @cell[ 1], @cell[ 2], @cell[ 9], @cell[10], @cell[11], @cell[18], @cell[19], @cell[20]]
-      @group[19] = [@cell[ 3], @cell[ 4], @cell[ 5], @cell[12], @cell[13], @cell[14], @cell[21], @cell[22], @cell[23]]
-      @group[20] = [@cell[ 6], @cell[ 7], @cell[ 8], @cell[15], @cell[16], @cell[17], @cell[24], @cell[25], @cell[26]]
-      @group[21] = [@cell[27], @cell[28], @cell[29], @cell[36], @cell[37], @cell[38], @cell[45], @cell[46], @cell[47]]
-      @group[22] = [@cell[30], @cell[31], @cell[32], @cell[39], @cell[40], @cell[41], @cell[48], @cell[49], @cell[50]]
-      @group[23] = [@cell[33], @cell[34], @cell[35], @cell[42], @cell[43], @cell[44], @cell[51], @cell[52], @cell[53]]
-      @group[24] = [@cell[54], @cell[55], @cell[56], @cell[63], @cell[64], @cell[65], @cell[72], @cell[73], @cell[74]]
-      @group[25] = [@cell[57], @cell[58], @cell[59], @cell[66], @cell[67], @cell[68], @cell[75], @cell[76], @cell[77]]
-      @group[26] = [@cell[60], @cell[61], @cell[62], @cell[69], @cell[70], @cell[71], @cell[78], @cell[79], @cell[80]]
+      @group[18].push(@cell[ 0], @cell[ 1], @cell[ 2], @cell[ 9], @cell[10], @cell[11], @cell[18], @cell[19], @cell[20])
+      @group[19].push(@cell[ 3], @cell[ 4], @cell[ 5], @cell[12], @cell[13], @cell[14], @cell[21], @cell[22], @cell[23])
+      @group[20].push(@cell[ 6], @cell[ 7], @cell[ 8], @cell[15], @cell[16], @cell[17], @cell[24], @cell[25], @cell[26])
+      @group[21].push(@cell[27], @cell[28], @cell[29], @cell[36], @cell[37], @cell[38], @cell[45], @cell[46], @cell[47])
+      @group[22].push(@cell[30], @cell[31], @cell[32], @cell[39], @cell[40], @cell[41], @cell[48], @cell[49], @cell[50])
+      @group[23].push(@cell[33], @cell[34], @cell[35], @cell[42], @cell[43], @cell[44], @cell[51], @cell[52], @cell[53])
+      @group[24].push(@cell[54], @cell[55], @cell[56], @cell[63], @cell[64], @cell[65], @cell[72], @cell[73], @cell[74])
+      @group[25].push(@cell[57], @cell[58], @cell[59], @cell[66], @cell[67], @cell[68], @cell[75], @cell[76], @cell[77])
+      @group[26].push(@cell[60], @cell[61], @cell[62], @cell[69], @cell[70], @cell[71], @cell[78], @cell[79], @cell[80])
+
+# pp @group
+      @cell.each do |c|
+        c.groups = find_groups(c)
+      end
+
+      #  debug
+      @group.each do |g|
+        print "#{g.id}: "
+        g.each do |cell|
+          print "(#{cell.x}, #{cell.y}), "
+        end
+        print "\n"
+      end
+
+    end
+
+    # cellが所属しているグループの配列を返す
+    def find_groups(cell)
+      groups = []
+      @group.each do |g|
+        if g.include? cell
+          groups << g
+        end
+      end
+      groups
     end
 
     def load(filename)
@@ -64,13 +98,25 @@ module Sudoku
           line.each_char do |c|
             cell = @cell[y*9+x]
             if c != '.'
-              cell.value = c.to_i
+              cell.value      = c.to_i
               cell.candidates = []
             end
             x += 1
           end
           y += 1
         end
+      end
+    end
+
+    def dump
+      n = 0
+      @group.each do |g|
+        print "group #{n}: "
+        g.each do |cell|
+          print "#{cell.candidates}, "
+        end
+        n += 1
+        puts ""
       end
     end
 
@@ -84,18 +130,6 @@ module Sudoku
         end
         puts ""
       end
-
-      n = 0
-      @group.each do |g|
-        puts n
-        g.each do |cell|
-          print "  #{cell.candidates}\n"
-        end
-        n += 1
-      end
-
-
-
     end
 
     def solved?
@@ -108,53 +142,48 @@ module Sudoku
     def exec_step
       value_changed = false
 
+      # 基本フィルタ (確定候補をもとにふるい落とす)
+      filter0()
+
+      # コンビネーション2のふるい
       @group.each do |g|
-        scan_group(g)
+        filter_combination2(g)
       end
     end
 
-    def scan_group(base_group)
+    # 基本フィルタ。候補が一つだけのセルは確定
+    def filter0()
+      @cell.each do |cell|
+        next if cell.value != 0 # すでに確定しているセルは除外
 
-      # 基本フィルタ。候補が一つだけのセルは確定
-      base_group.each do |c|
-        next if c.candidates.count == 0
-
-        candidates = [1,2,3,4,5,6,7,8,9]
-        find_groups(c).each do |g|
-          g.each do |cell|
-            next if cell.value == 0
-            candidates.delete(cell.value)
+pp "cell(#{cell.x}, #{cell.y}), candidates: #{cell.candidates}"
+        # cellが所属しているグループの
+        cell.groups.each do |group|
+          # 各セルについて、確定していたらそのセルの値を候補から除外
+pp " looking group #{group.id}"
+          group.each do |c|
+            next if c.value == 0
+            cell.candidates.delete(c.value)
+pp "  delete value #{c.value}"
           end
         end
 
-        c.candidates = candidates
+pp " updated candidates: #{cell.candidates}"
 
-        if candidates.count == 1
-          # 確定
-          c.value = candidates[0]
-          c.candidates = []
-          value_changed = TRUE
+        # 確定したら更新
+        if cell.candidates.count == 1
+          cell.value      = cell.candidates[0]
+          cell.candidates = []
+pp " fixed: #{cell.value}"
+          value_changed   = TRUE
         end
       end
-
-      # コンビネーション2のふるい
-#      filter_combination2(group)
-      # 候補1のセルを確定とする
     end
 
-    # base_cellが所属しているグループの配列を返す
-    def find_groups(base_cell)
-      groups = []
-      @group.each do |g|
-        if g.include? base_cell
-          groups << g
-        end
-      end
-      groups
-    end
-
+    # 同一のn個の候補を持つセル→そのセルがn個ならそれらのセルでその候補値を専有できる。グループ内でその候補値は除外
     def filter_combination2(base_group)
     end
+
 
   end
 end
@@ -168,14 +197,20 @@ if __FILE__ == $0
 
   solver.load("ex1.sudoku")
 
-  while TRUE
-    puts "----"
     solver.show
+  n = 0
+  while TRUE
+    puts ""
+    puts "#{n}"
+    puts "--------------------"
+
     solver.exec_step
+    solver.show
+    solver.dump
     break unless solver.value_changed
   end
 
-  puts "----\n done"
+  puts "--------------------\ndone."
   solver.show
 
 end
